7e9e619eff19d3c967e598eaec04a09470dbb15a
JessicaGreben <jessica.greben1+github@gmail.com>
2018-05-28 07:44:31 -0700

    add version command

---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var Version = "v0.1.0"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print pratique version",
+	Run: getVersion,
+}
+
+func getVersion(cmd *cobra.Command, args []string) {
+	fmt.Println("Current API version:", Version)
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
