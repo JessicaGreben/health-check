5c3ed2ba5802c0f6590c06db7187a7797213823d
JessicaGreben <jessica.greben1+github@gmail.com>
2018-10-10 07:37:27 -0700

    open report in browser

--- pkg/report/create.go
+++ pkg/report/create.go
@@ -1,8 +1,10 @@
 package report
 
 import (
+	"fmt"
 	"html/template"
 	"os"
+	"os/exec"
 
 	"github.com/jessicagreben/health-check/pkg/types"
 )
@@ -23,8 +25,14 @@ func Render(report types.Results) error {
 	if err := tmpl.Execute(fd, report); err != nil {
 		return err
 	}
+
+	open("./report.html")
 	return nil
 }
 
-// Open opens a file in the browser.
-func Open(path string) {}
+// open opens a file in the browser.
+func open(path string) {
+	if err := exec.Command("/usr/bin/open", path).Run(); err != nil {
+		fmt.Printf("report.Open err: %x\n", err.Error())
+	}
+}
