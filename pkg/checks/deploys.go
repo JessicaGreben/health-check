b3e944f7736c80523ecea50d0bd5554b70f0dcea
Micah Huber <micah.huber@mailbox.org>
2018-10-12 17:18:28 -0400

    Added some initial check stuff for hostPorts.

--- pkg/checks/deploys.go
+++ pkg/checks/deploys.go
@@ -3,6 +3,7 @@ package checks
 import (
 	"bytes"
 	"fmt"
+	"reflect"
 
 	appsv1 "k8s.io/api/apps/v1"
 )
@@ -14,7 +15,7 @@ func labels(deployment appsv1.Deployment) (bool, string) {
 	var lblExists bool
 
 	collLabels := map[string]map[string]string{"deployment": deployment.GetObjectMeta().GetLabels(), "pod": deployment.Spec.Template.GetLabels()}
-
+	deployment.Spec.Templ
 	for _, lbl := range [1]string{"app"} {
 		for lblFrom := range collLabels {
 			_, lblExists = collLabels[lblFrom][lbl]
@@ -25,4 +26,30 @@ func labels(deployment appsv1.Deployment) (bool, string) {
 		}
 	}
 	return violation, msg.String()
+}
+
+// check for any pods in deployment spec using hostPort
+func hostPort(deployment appsv1.Deployment) (bool, string) {
+	for container := range deployment.Spec.Template.Spec.Containers {
+		for port := range container.Ports {
+			//TODO: check if HostPort is defined
+		}
+	}
+}
+
+// check if a struct field is omitted.  I have no idea if this will work.
+func StructFieldExists(IFace interface{}, FieldName string) bool {
+	ValueIface := reflect.ValueOf(Iface)
+
+	//check if passed interface is a pointer
+	if ValueIface.Type().Kind() != reflect.Ptr {
+		ValueIface = reflect.New(reflect.TypeOf(Iface))
+	}
+
+	// deref with Elem() and get field by name
+	Field := ValueIface.Elem().FieldByName(FieldName)
+	if !Field.IsValid() {
+		return false
+	}
+	return true
 }
