ca40ee8c479872784040c847064baead1e392006
jessicagreben <Jessica.greben1+github@gmail.com>
2018-10-16 09:21:36 -0700

    add pod and deploy subcommands

--- cmd/run.go
+++ cmd/run.go
@@ -14,15 +14,16 @@ var runCmd = &cobra.Command{
 	Short: "Check the health of the current Kubernetes cluster",
 	Long:  `Run a health check against a Kubernetes cluster. A report is generated describing the results of the checks.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
 		podResults, err := checks.Pods()
 		if err != nil {
 			fmt.Print("checks.Pods err: ", err)
+			return
 		}
 
 		deployResults, err := checks.Deploys()
 		if err != nil {
 			fmt.Print("checks.Pods err: ", err)
+			return
 		}
 
 		results := types.Results{
@@ -32,10 +33,57 @@ var runCmd = &cobra.Command{
 
 		if err := report.Render(results); err != nil {
 			fmt.Print("report.Render err: ", err)
+			return
+		}
+	},
+}
+
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Run deployment health checks.",
+	Long:  `Check the health of the current Kubernetes cluster by executing the health checks for deployments.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		deployResults, err := checks.Deploys()
+		if err != nil {
+			fmt.Print("checks.Pods err: ", err)
+			return
+		}
+
+		results := types.Results{
+			Deploys: deployResults,
+		}
+
+		if err := report.Render(results); err != nil {
+			fmt.Print("report.Render err: ", err)
+			return
+		}
+	},
+}
+
+var podCmd = &cobra.Command{
+	Use:   "pod",
+	Short: "Run pod health checks.",
+	Long:  `Check the health of the current Kubernetes cluster by executing the health checks for pods.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		podResults, err := checks.Pods()
+		if err != nil {
+			fmt.Print("checks.Pods err: ", err)
+			return
+		}
+
+		results := types.Results{
+			Pods: podResults,
+		}
+
+		if err := report.Render(results); err != nil {
+			fmt.Print("report.Render err: ", err)
+			return
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
+	runCmd.AddCommand(deployCmd)
+	runCmd.AddCommand(podCmd)
 }
